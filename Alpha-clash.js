1b7e58cdd3d59b1e97f3b93d555133477cf6c8a8
Meharaz <maharazbhuiyan3@gmail.com>
2024-08-09 03:40:56 +0600

    full work done

--- Alpha-clash.js
+++ Alpha-clash.js
@@ -11,8 +11,9 @@ let randomNumber=()=>{
     let str="abcdefghijklmnopqrstuvwxyz";
     let arr=str.split('');
     let random=Math.round(Math.random()*25)
-    console.log(random);
+    // console.log(random);
     let alphabhet=arr[random];
+    console.log(`the random alphabhet is ${alphabhet}`)
     return alphabhet
 }
  function setBackgroundColor(ele){
@@ -21,3 +22,24 @@ let randomNumber=()=>{
     
 
  }
+ function removeBackgroundColor(ele){
+    let ID=document.getElementById(ele);
+    ID.classList.remove("bg-yellow-600")
+    
+
+ }
+ let setScoreId=(ele)=>{
+    let ID=document.getElementById(ele);
+    let score=ID.innerText;
+    score=parseInt(score);
+    return score;
+ }
+ let textId=(ele,num)=>{
+    let Id=document.getElementById(ele);
+    Id.innerText=num;
+ }
+    
+let FinalScore=(ele,score)=>{
+    let ID=document.getElementById(ele);
+    ID.innerText=score;
+}
--- utility.js
+++ utility.js
@@ -1,3 +1,41 @@
+
+    document.addEventListener('keyup',(ele)=>{
+        let key=ele.key
+        if(key==="Escape"){
+            console.log("QUIT");
+            gameOver()
+        }
+        let get=document.getElementById("Alphabhet");
+        let l=get.innerText;
+        let letter=l.toLowerCase();
+        console.log(key,letter)
+        if(key===letter){
+            console.log("you won.you get a point ");
+            removeBackgroundColor(letter)
+            // update score
+            let score=setScoreId("current-score")
+             let new_score=score+1;
+             textId("current-score",new_score)
+            //  play again
+             countinuePlay(); 
+            //  final score shown
+            FinalScore("last_score",new_score)
+            }
+           else{
+               console.log('you lose.you lost a life');
+               // update life
+               let life_number=setScoreId('score_Life')
+               let newLifeScore=life_number-1;
+               textId('score_Life',newLifeScore)
+               if(newLifeScore===0){
+                gameOver()
+               }
+           }
+
+    })
+
+
+
 let countinuePlay=()=>{
     let alpha=randomNumber();
     let ID=document.getElementById("Alphabhet");
@@ -8,6 +46,13 @@ let countinuePlay=()=>{
 
 function Play(){
     setHiddenById('home');
+    setHiddenById("final_score")
     getDisplayById("playground")
+    textId("score_Life",7);
+    textId("current-score",0)
     countinuePlay()
+}
+function gameOver(){
+    setHiddenById("playground");
+    getDisplayById("final_score")
 }
